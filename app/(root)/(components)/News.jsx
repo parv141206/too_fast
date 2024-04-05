e399b66fc7002f6ad977e9d68b0e3d445e8daa5d
parv141206 <148616575+parv141206@users.noreply.github.com>
2024-04-05 22:57:25 +0530

    Final normal commit

--- app/(root)/(components)/News.jsx
+++ app/(root)/(components)/News.jsx
@@ -11,8 +11,8 @@ export default function News() {
   // Memoize the fetching logic
   const fetchedData = useMemo(() => {
     const fetchData = async () => {
-      const response = await fetch(".netlify/functions/divyaBhaskar");
-      // const response2 = await fetch("/api/gs");
+      const response = await fetch("http://localhost:3000/api/db");
+      const response2 = await fetch("http://localhost:3000/api/gs");
       const data = await response.json();
       // const data2 = await response2.json();
       const articles = data.articles;
